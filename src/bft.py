ad91213037697061b5341e1b9adbf3c68ddda9d1
Thomas Mailund <mailund@birc.au.dk>
2022-09-22 10:53:36 +0200

    Match in bft

--- src/bft.py
+++ src/bft.py
@@ -12,4 +12,20 @@ def bf_order(t: T | None) -> Iterable[int]:
     >>> list(bf_order(tree))
     [2, 1, 4, 3, 5]
     """
-    return  # FIXME
+    # When we have a tree on the stack we need to traverse it,
+    # when we have an int it is a value we should emit
+    queue: deque[T | int] = deque()
+
+    def enqueue(x: int | T | None) -> None:
+        if x is not None:
+            queue.append(x)
+
+    enqueue(t)
+    while queue:
+        match queue.pop():
+            case int(v):
+                yield v
+            case T(val=val, left=left, right=right):
+                yield val
+                enqueue(right)
+                enqueue(left)
